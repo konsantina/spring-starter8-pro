b6526ec1ad4f3ad52652e93643feb64292615a70
Κωνσταντίνα Νίκα <kwnstantinanika@gmail.com>
2025-07-01 15:22:13 +0300

    initial commit

---
+++ src/main/java/gr/aueb/cf/spring_starter8_pro/SpringStarter8ProApplication.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.spring_starter8_pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStarter8ProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStarter8ProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gr/aueb/cf/spring_starter8_pro/SpringStarter8ProApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.spring_starter8_pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStarter8ProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
